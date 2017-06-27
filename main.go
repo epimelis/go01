dae2ca188b93bd66de3dc9091003c02827fc8a37
A Yong <andraceyong@gmail.com>
2017-06-27 17:16:24 -0400

    commit

--- main.go
+++ main.go
@@ -24,10 +24,33 @@ func main(){
 	fmt.Println(*arrayPtr1[0])
 
 	fmt.Println("-----Assign one array to another-----")
-	var array5 [2] string
-	var array6[2]string{"red", "blue"}
+	var array5 [2]string
+	var array6 =[2]string{"red", "blue"}
+
 	array5=array6
 	fmt.Println(array5)
 	fmt.Println(array6)
+
+	fmt.Println("-----Assign one pointer array to another-----")
+
+	var arrayPtr2A =[2]*string{new(string), new(string)}
+	var arrayPtr2B =[2]*string{new(string), new(string)}
+	*arrayPtr2A[0]="ppp"
+	*arrayPtr2A[1]="qqq"
+	fmt.Println(arrayPtr2A[0])
+	fmt.Println(arrayPtr2B[0])
+	fmt.Println("-----")
+	fmt.Println(*arrayPtr2A[0])
+	fmt.Println(*arrayPtr2B[0])
+	fmt.Println("-----")
+	arrayPtr2B=arrayPtr2A
+	fmt.Println(*arrayPtr2A[0])
+	fmt.Println(*arrayPtr2B[0])
+	fmt.Println("-----")
+
+	fmt.Println("-----Multidimensional array----------------")
+
+	var arrayMulti_A =[2][3]int{{10,10},{20,20}}
+	fmt.Println(arrayMulti_A)
 
 }
